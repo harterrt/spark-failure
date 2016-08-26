f92e2597c209a269a9ea7d457cc572f8de78139e
Ryan Harter <harterrt@gmail.com>
2016-08-26 01:38:51 +0000

    Adding fixes for failing case

---
+++ fixed.scala
@@ -0,0 +1,35 @@
+package com.mozilla.telemetry.views
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.hive.HiveContext
+
+case class Base (
+    client_id: String
+)
+
+case class Target (
+    client_id: String
+)
+
+object FailingView {
+  def generateCrossSectional(base: Base) = {
+    Target(base.client_id)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName(this.getClass.getName)
+    sparkConf.setMaster(sparkConf.get("spark.master", "local[*]"))
+    val sc = new SparkContext(sparkConf)
+
+    val hiveContext = new HiveContext(sc)
+    import hiveContext.implicits._
+
+    val ds = hiveContext
+      .sql("SELECT * FROM longitudinal")
+      .selectExpr("client_id")
+      .as[Base]
+    val output = ds.map(generateCrossSectional)
+
+    println("="*80 + "\n" + output.count + "\n" + "="*80)
+  }
+}
---
+++ inline.scala
@@ -0,0 +1,31 @@
+package com.mozilla.telemetry.views
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.hive.HiveContext
+
+case class Base (
+    client_id: String
+)
+
+case class Target (
+    client_id: String
+)
+
+object FailingView {
+  val sparkConf = new SparkConf().setAppName(this.getClass.getName)
+  sparkConf.setMaster(sparkConf.get("spark.master", "local[*]"))
+  val sc = new SparkContext(sparkConf)
+
+  val hiveContext = new HiveContext(sc)
+  import hiveContext.implicits._
+
+  def main(args: Array[String]): Unit = {
+    val ds = hiveContext
+      .sql("SELECT * FROM longitudinal")
+      .selectExpr("client_id")
+      .as[Base]
+    val output = ds.map(xx => Target(xx.client_id))
+
+    println("="*80 + "\n" + output.count + "\n" + "="*80)
+  }
+}
